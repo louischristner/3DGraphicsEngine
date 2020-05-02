642ff8bca282d76a1c424b9717533f151cd5153a
Louis <louis.christner@epitech.eu>
2020-05-02 18:57:53 +0200

    Create structures

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 ** main
 */
 
+#include <vector>
+
+struct Vect3D {
+    float x, y, z;
+};
+
+struct Triangle {
+    Vect3D p[3];
+};
+
+struct Mesh {
+    std::vector<Triangle> tris;
+};
+
+struct Matrix4x4 {
+    float m[4][4] = {0};
+};
+
 int main(void)
 {
     return (0);
