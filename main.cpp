8a002abb462a6e09626e311ad4d20be9de6e608d
Louis <louis.christner@epitech.eu>
2020-05-04 20:05:24 +0200

    Display rotating square

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 ** main
 */
 
+#include <cmath>
+#include <chrono>
 #include <vector>
 
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
 
 struct Vect3D {
     float x, y, z;
@@ -27,7 +32,177 @@ struct Matrix4x4 {
     float m[4][4] = {0};
 };
 
-sf::ConvexShape createLine(sf::Vector2f p1, sf::Vector2f p2)
+class IGraphicsDrawer {
+    public:
+        virtual ~IGraphicsDrawer() = default;
+
+        virtual void onCreate() = 0;
+        virtual void onUpdate() = 0;
+};
+
+class SFMLDrawer : public IGraphicsDrawer {
+    public:
+        SFMLDrawer();
+        virtual ~SFMLDrawer() = default;
+
+        void onCreate() override;
+        void onUpdate() override;
+
+    protected:
+    private:
+        sf::Event _event;
+        sf::RenderWindow _window;
+        std::chrono::_V2::system_clock::time_point _start;
+
+        void drawTriangle(Triangle) noexcept;
+        sf::ConvexShape createLine(sf::Vector2f p1, sf::Vector2f p2) const noexcept;
+        void mutliplyMatrixVector(Vect3D &i, Vect3D &o, Matrix4x4 &m) const noexcept;
+
+        float _fTheta = 0;
+
+        Mesh _meshCube;
+        Matrix4x4 _matProj;
+};
+
+SFMLDrawer::SFMLDrawer(): _window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My Window")
+{
+    this->onCreate();
+    while (_window.isOpen())
+        this->onUpdate();
+}
+
+void SFMLDrawer::onCreate()
+{
+
+    _window.setFramerateLimit(30);
+    _start = std::chrono::system_clock::now();
+
+    _meshCube.tris = {
+
+        // SOUTH
+        { 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+        // EAST
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+        // NORTH
+        { 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+        // WEST
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+        // TOP
+        { 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+        { 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+        // BOTTOM
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+        { 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+    };
+
+    float fNear = 0.1f;
+    float fFar = 1000.0f;
+    float fFov = 90.0f;
+    float fAspectRatio = (float)WINDOW_HEIGHT / (float)WINDOW_WIDTH;
+    float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * M_PI);
+
+    _matProj.m[0][0] = fAspectRatio * fFovRad;
+    _matProj.m[1][1] = fFovRad;
+    _matProj.m[2][2] = fFar / (fFar - fNear);
+    _matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+    _matProj.m[2][3] = 1.0f;
+    _matProj.m[3][3] = 0.0f;
+}
+
+void SFMLDrawer::onUpdate()
+{
+    while (_window.pollEvent(_event))
+        if (_event.type == sf::Event::Closed)
+            _window.close();
+
+    _window.clear(sf::Color::Black);
+
+    Matrix4x4 matRotX;
+    Matrix4x4 matRotZ;
+
+    float fElapsedTime = (std::chrono::system_clock::now() - _start).count() / 1000000000.0f;
+    _fTheta += 1.0f * fElapsedTime;
+
+    _start = std::chrono::system_clock::now();
+
+    // Rotation X
+    matRotX.m[0][0] = 1;
+    matRotX.m[1][1] = cosf(_fTheta * 0.5f);
+    matRotX.m[1][2] = sinf(_fTheta * 0.5f);
+    matRotX.m[2][1] = -sinf(_fTheta * 0.5f);
+    matRotX.m[2][2] = cosf(_fTheta * 0.5f);
+    matRotX.m[3][3] = 1;
+
+    // Rotation Y
+    matRotZ.m[0][0] = cosf(_fTheta);
+    matRotZ.m[0][1] = sinf(_fTheta);
+    matRotZ.m[1][0] = -sinf(_fTheta);
+    matRotZ.m[1][1] = cosf(_fTheta);
+    matRotZ.m[2][2] = 1;
+    matRotZ.m[3][3] = 1;
+
+    for (auto tri : _meshCube.tris) {
+        Triangle triRotatedZ;
+        Triangle triRotatedZX;
+        Triangle triTranslated;
+        Triangle triProjected;
+
+        mutliplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+        mutliplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+        mutliplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+        mutliplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+        mutliplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+        mutliplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+        triTranslated = triRotatedZX;
+        triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+        triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+        triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+        mutliplyMatrixVector(triTranslated.p[0], triProjected.p[0], _matProj);
+        mutliplyMatrixVector(triTranslated.p[1], triProjected.p[1], _matProj);
+        mutliplyMatrixVector(triTranslated.p[2], triProjected.p[2], _matProj);
+
+        // Scale into view
+        triProjected.p[0].x += 1.0f;
+        triProjected.p[0].y += 1.0f;
+        triProjected.p[1].x += 1.0f;
+        triProjected.p[1].y += 1.0f;
+        triProjected.p[2].x += 1.0f;
+        triProjected.p[2].y += 1.0f;
+
+        triProjected.p[0].x *= 0.5f * (float)WINDOW_WIDTH;
+        triProjected.p[0].y *= 0.5f * (float)WINDOW_HEIGHT;
+        triProjected.p[1].x *= 0.5f * (float)WINDOW_WIDTH;
+        triProjected.p[1].y *= 0.5f * (float)WINDOW_HEIGHT;
+        triProjected.p[2].x *= 0.5f * (float)WINDOW_WIDTH;
+        triProjected.p[2].y *= 0.5f * (float)WINDOW_HEIGHT;
+
+        this->drawTriangle(triProjected);
+    }
+
+    _window.display();
+}
+
+void SFMLDrawer::drawTriangle(Triangle tri) noexcept
+{
+    _window.draw(this->createLine(sf::Vector2f(tri.p[0].x, tri.p[0].y), sf::Vector2f(tri.p[1].x, tri.p[1].y)));
+    _window.draw(this->createLine(sf::Vector2f(tri.p[1].x, tri.p[1].y), sf::Vector2f(tri.p[2].x, tri.p[2].y)));
+    _window.draw(this->createLine(sf::Vector2f(tri.p[2].x, tri.p[2].y), sf::Vector2f(tri.p[0].x, tri.p[0].y)));
+}
+
+sf::ConvexShape SFMLDrawer::createLine(sf::Vector2f p1, sf::Vector2f p2) const noexcept
 {
     sf::ConvexShape convexShape(4);
 
@@ -42,12 +217,29 @@ sf::ConvexShape createLine(sf::Vector2f p1, sf::Vector2f p2)
     return convexShape;
 }
 
+void SFMLDrawer::mutliplyMatrixVector(Vect3D &i, Vect3D &o, Matrix4x4 &m) const noexcept
+{
+    float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+    o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+    o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+    o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+
+    if (w != 0.0f) {
+        o.x /= w;
+        o.y /= w;
+        o.z /= w;
+    }
+}
+
 int main(void)
 {
+    SFMLDrawer drawer;
+
     /**
 
     sf::Event event;
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My Window");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "My Window");
 
     while (window.isOpen()) {
         while (window.pollEvent(event))
