b55dfef383a0c841a249e6f19846537a9ee9ae93
Louis Christner <louis.christner@epitech.eu>
2020-12-15 21:48:14 +0100

    Create mesh object from obj file
    Contains warning (deprecated header)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 #include <chrono>
 #include <vector>
 
+#include <fstream>
+#include <strstream>
+
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -33,7 +36,45 @@ struct Triangle {
 
 struct Mesh {
     std::vector<Triangle> tris;
+
+    bool loadFromObjFile(const std::string filename);
 };
+
+bool Mesh::loadFromObjFile(const std::string filename)
+{
+    std::ifstream file(filename);
+
+    if (!file.is_open())
+        return false;
+
+    std::string line;
+    std::vector<Vect3D> verts;
+
+    while (std::getline(file, line)) {
+        std::strstream stream;
+        stream << line;
+
+        char junk;
+
+        if (line[0] == 'v') {
+            Vect3D tmpV;
+
+            stream >> junk >> tmpV.x >> tmpV.y >> tmpV.z;
+            verts.push_back(tmpV);
+        } else if (line[0] == 'f') {
+            int indexes[3];
+
+            stream >> junk >> indexes[0] >> indexes[1] >> indexes[2];
+            tris.push_back({
+                verts[indexes[0] - 1],
+                verts[indexes[1] - 1],
+                verts[indexes[2] - 1]
+            });
+        }
+    }
+
+    return true;
+}
 
 struct Matrix4x4 {
     float m[4][4] = {0};
