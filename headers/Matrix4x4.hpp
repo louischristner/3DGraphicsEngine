c04709db5ad879dd8acd07ef0028ff6bba482c19
Louis Christner <louis.christner@epitech.eu>
2021-01-14 20:21:29 +0100

    Update Matrix4x4
    Add createPointAt static method & quickInverse method

--- headers/Matrix4x4.hpp
+++ headers/Matrix4x4.hpp
@@ -15,6 +15,8 @@
 struct Matrix4x4 {
     float m[4][4] = {0};
 
+    Matrix4x4 quickInverse(void) const noexcept;
+
     Vect3D operator*(const Vect3D &v) const;
     Matrix4x4 operator*(const Matrix4x4 &m2) const;
 
@@ -24,6 +26,7 @@ struct Matrix4x4 {
     static Matrix4x4 createRotationZ(float fAngleRad);
     static Matrix4x4 createTranslation(float x, float y, float z);
     static Matrix4x4 createProjection(float fFovDegrees, float fAspectRatio, float fNear, float fFar);
+    static Matrix4x4 createPointAt(const Vect3D &pos, const Vect3D &target, const Vect3D &up);
 };
 
 #endif /* !MATRIX4X4_HPP_ */
--- src/Matrix4x4.cpp
+++ src/Matrix4x4.cpp
@@ -7,6 +7,39 @@
 
 #include "../headers/Matrix4x4.hpp"
 
+/**
+ * Quick implementation of matrices inverser
+ *
+ * @return the inverse of the matrix
+ * @deprecated this is only for rotation & translation matrices
+ */
+Matrix4x4 Matrix4x4::quickInverse(void) const noexcept
+{
+    Matrix4x4 matrix;
+
+    matrix.m[0][0] = m[0][0];
+    matrix.m[0][1] = m[1][0];
+    matrix.m[0][2] = m[2][0];
+    matrix.m[0][3] = 0.0f;
+
+    matrix.m[1][0] = m[0][1];
+    matrix.m[1][1] = m[1][1];
+    matrix.m[1][2] = m[2][1];
+    matrix.m[1][3] = 0.0f;
+
+    matrix.m[2][0] = m[0][2];
+    matrix.m[2][1] = m[1][2];
+    matrix.m[2][2] = m[2][2];
+    matrix.m[2][3] = 0.0f;
+
+    matrix.m[3][0] = -(m[3][0] * matrix.m[0][0] + m[3][1] * matrix.m[1][0] + m[3][2] * matrix.m[2][0]);
+    matrix.m[3][1] = -(m[3][0] * matrix.m[0][1] + m[3][1] * matrix.m[1][1] + m[3][2] * matrix.m[2][1]);
+    matrix.m[3][2] = -(m[3][0] * matrix.m[0][2] + m[3][1] * matrix.m[1][2] + m[3][2] * matrix.m[2][2]);
+    matrix.m[3][3] = 1.0f;
+
+    return matrix;
+}
+
 Vect3D Matrix4x4::operator*(const Vect3D &v) const
 {
     float w = v.x * m[0][3] + v.y * m[1][3] + v.z * m[2][3] + m[3][3];
@@ -105,5 +138,41 @@ Matrix4x4 Matrix4x4::createProjection(float fFovDegrees, float fAspectRatio, flo
     matrix.m[3][2] = (-fFar * fNear) / (fFar - fNear);
     matrix.m[2][3] = 1.0f;
     matrix.m[3][3] = 0.0f;
+    return matrix;
+}
+
+Matrix4x4 Matrix4x4::createPointAt(const Vect3D &pos, const Vect3D &target, const Vect3D &up)
+{
+    // Calculate new forward direction
+    Vect3D newForward = (target - pos).normalize();
+
+    // Calculate new up direction
+    Vect3D newUp = (up - (newForward * up.dotProduct(newForward))).normalize();
+
+    // Calculate new right direction
+    Vect3D newRight = newUp.crossProduct(newForward);
+
+    Matrix4x4 matrix;
+
+    matrix.m[0][0] = newRight.x;
+    matrix.m[0][1] = newRight.y;
+    matrix.m[0][2] = newRight.z;
+    matrix.m[0][3] = .0f;
+
+    matrix.m[1][0] = newUp.x;
+    matrix.m[1][1] = newUp.y;
+    matrix.m[1][2] = newUp.z;
+    matrix.m[1][3] = .0f;
+
+    matrix.m[2][0] = newForward.x;
+    matrix.m[2][1] = newForward.y;
+    matrix.m[2][2] = newForward.z;
+    matrix.m[2][3] = .0f;
+
+    matrix.m[3][0] = pos.x;
+    matrix.m[3][1] = pos.y;
+    matrix.m[3][2] = pos.z;
+    matrix.m[3][3] = 1.0f;
+
     return matrix;
 }
